4fc62ae3d4870b18d7adb215b8884586b30ab75e
lucasmussi27 <lucas.mussi27@outlook.com>
2019-06-17 13:53:09 -0300

    Updates

---
+++ precache-manifest.f74af7c75f81ca8428280ff0972c3b48.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30022fafc1e96179986c24bdf5b6f5cb",
+    "url": "/t02/index.html"
+  },
+  {
+    "revision": "50f57461eb6b6a26be68",
+    "url": "/t02/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "19288321642dab330105",
+    "url": "/t02/static/css/main.fb472e9e.chunk.css"
+  },
+  {
+    "revision": "50f57461eb6b6a26be68",
+    "url": "/t02/static/js/2.c8dbbb81.chunk.js"
+  },
+  {
+    "revision": "19288321642dab330105",
+    "url": "/t02/static/js/main.8f6b7372.chunk.js"
+  },
+  {
+    "revision": "74ef6ce4908f64504a1b",
+    "url": "/t02/static/js/runtime~main.6a9f2f68.js"
+  },
+  {
+    "revision": "ec1de54a7bc515888d530bdd5cc554c1",
+    "url": "/t02/static/media/bilhar.ec1de54a.png"
+  },
+  {
+    "revision": "ab5e73ff44ba100d9fe3fa9179eaaf2d",
+    "url": "/t02/static/media/coding.ab5e73ff.png"
+  },
+  {
+    "revision": "0081b52a86bef221259d079105fe8a8f",
+    "url": "/t02/static/media/old-tape.0081b52a.png"
+  },
+  {
+    "revision": "0231a9478650d65f5ad59c15690fa0e8",
+    "url": "/t02/static/media/papel.0231a947.png"
+  },
+  {
+    "revision": "1d835ae076b0bf7d3f9727c27133eec5",
+    "url": "/t02/static/media/perfil-img.1d835ae0.png"
+  }
+]);
